dafa4e9975549e25db05dc489ea7b8fe466ed4ab
Hichatu <realhichatu@gmail.com>
2022-05-01 16:50:12 +0100

    asd
    asd

--- javascript/savegame.js
+++
@@ -1,9 +0,0 @@
-//Savegame garbage
-var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem("klickClickerSave", JSON.stringify(gameData))
-}, 1000)
-
-var savegame = JSON.parse(localStorage.getItem("klickClickerSave"))
-if (savegame !== null) {
-  gameData = savegame
-}
